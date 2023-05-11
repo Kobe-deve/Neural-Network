e45614c684bc256e1f2ecbdbac1b498f14a75e5a
Kobe <koberunnels@gmail.com>
2023-05-11 11:26:25 -0500

    added initialization of input/output layer and added READ_ME

---
+++ READ_ME.md
@@ -0,0 +1,8 @@
+C Neural Network
+
+File input format:
+	-Files
+		-(File Name)_input.txt
+		-(File Name)_output.txt
+		
+		
--- main.c
+++ main.c
@@ -4,40 +4,56 @@
 
 struct node
 {
+	int activation;
+	
+	int bias;
 	
 };
 
+// Neural Network data structures 
+
+// array for the input/output layers
 struct node * inputs;
 struct node * output;
 
+// array for hidden layers
+struct node ** hiddenLayer;
+
+// Training data 
+
 int ** trainingData; // specific array of training data
+int ** trainingDataLabels; // labels for training data 
 
 int inputSize; // the size of one set of training data 
 int numInDataFile; // number of sets in training data file
-
-int ** trainingDataLabels; // labels for training data 
-
 int labelSize; // the size of the labels 
 
 // reads the input file 
 void readTestInputFiles(char * fileName)
 {
 	FILE *readFile;
+	FILE *readOutputFile;
 	char * fileReader = malloc(128 * sizeof(char)); 
 	int i,j;
 
 	char inputFile[128] = "";
 	char outputFile[128] = "";
 	
-	// read training data input file 
+	// set up reading training data input file 
 	strcat(inputFile,fileName);
 	strcat(inputFile,"_input.txt");
 	
 	readFile = fopen(inputFile,"r");
+	
+	// set up reading training label output file 
+	strcat(outputFile,fileName);
+	strcat(outputFile,"_output.txt");
+	
+	readOutputFile = fopen(outputFile,"r");
 
-	if(readFile == NULL)
+	if(readFile == NULL || readOutputFile == NULL)
 	{
-		printf("\nERROR: UNABLE TO OPEN FILE");
+		printf("\nERROR: UNABLE TO OPEN FILES");
 		
 		return;
 	}
@@ -69,14 +85,10 @@ void readTestInputFiles(char * fileName)
 				trainingData[i][j] = atoi(fileReader);
 			}
 		}
-			
-		// read training label output file 
-		strcat(outputFile,fileName);
-		strcat(outputFile,"_output.txt");
-	
-		readFile = fopen(outputFile,"r");
 		
-		fscanf(readFile,"%s",fileReader);
+		// read training label file
+		
+		fscanf(readOutputFile,"%s",fileReader);
 		labelSize = atoi(fileReader);
 		
 		// allocate training array
@@ -94,7 +106,7 @@ void readTestInputFiles(char * fileName)
 			// read specific set
 			for(j=0;j<labelSize;j++)
 			{
-				fscanf(readFile,"%s",fileReader);
+				fscanf(readOutputFile,"%s",fileReader);
 				trainingDataLabels[i][j] = atoi(fileReader);
 			}
 		}
@@ -109,6 +121,7 @@ void main()
 	int i,j;
 	readTestInputFiles("test_data");
 	
+	/*
 	for(i=0;i<numInDataFile;i++)
 	{
 		for(j=0;j<inputSize;j++)
@@ -128,4 +141,9 @@ void main()
 		}
 		printf("\n");
 	}
+	*/
+
+	// after reading file, set up the input and output layers of the network
+	inputs = malloc(inputSize * sizeof(struct node));
+	output = malloc(labelSize * sizeof(struct node));
 }
